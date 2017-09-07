291c41fe20efafab7c68e03405a0841c09218f52
ivo692009 <ivo692009@gmail.com>
2017-09-07 13:54:21 -0300

    casi casi

--- ClienteForm.php
+++ ClienteForm.php
@@ -144,8 +144,74 @@
 
 		public function persona($id){
 
+			require "usuario.php";
+			error_reporting(E_ALL);
+			ini_set("display_errors", true);
+			header('Content-Type: text/html; charset=UTF-8');
+			try {
+			    //Coneccion a la base de datos
+			    $pdo = new PDO('mysql:host=localhost;dbname=clientes_db', $usuario, $contraseña);
+			    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
+			    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			    $pdo->exec("SET NAMES UTF8");
+			    //armamos el SQL
+			    $sql = "SELECT * FROM clientes WHERE id=:id";
+			    //preparamos un statement con el sql anterior
+			    $stmt = $pdo->prepare($sql);
+			    //especificamos la salida como un array
+			    $stmt->setFetchMode(PDO::FETCH_OBJ); //podria ser PDO::FETCH_OBJ
+			    //sustituimos los parametros con los valores reales
+			    $stmt->bindParam(':id', $id);
+			    //ejecutamos la consulta
+			    $stmt->execute();
+			     //recuperamos los datos en el array asoc.
+    			$persona = $stmt->fetchAll();
+			} catch (PDOException $e) {
+			    echo 'Error de la coneccion a la BD:' . $e->getMessage();
+			    die();
+			}
 
 			
+		}
+
+		public function modif(){
+
+			require "usuario.php";
+			error_reporting(E_ALL);
+			ini_set("display_errors", true);
+			//Se reciven valores por POST
+			$nombre_nuevo = $_POST['nombre'];
+			$apellido_nuevo = $_POST['apellido'];
+			$fechnac_nuevo = $_POST['fecha'];
+			$nacionalidad_id_nuevo = $_POST['categoria'];
+			$estado_nuevo = $_POST['vigente'];
+			$id = $_POST['id'];
+			header('Content-Type: text/html; charset=UTF-8');
+			try {
+			    //Coneccion a la base de datos
+			    $pdo = new PDO('mysql:host=localhost;dbname=clientes_db', $usuario, $contraseña);
+			    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
+			    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			    $pdo->exec("SET NAMES UTF8");
+			    //armamos el SQL
+			    $sql = "UPDATE `clientes` SET apellido=:apellido, nombre=:nombre, activo=:estado, fechnac=:fechnac, nacionalidad_id=:nacionalidad_id WHERE clientes.id=:id";
+			    //preparamos un statement con el sql anterior
+			    $stmt = $pdo->prepare($sql);
+			    //especificamos la salida como un array
+			    $stmt->setFetchMode(PDO::FETCH_OBJ); //podria ser PDO::FETCH_OBJ
+			    //sustituimos los parametros con los valores reales
+			    $stmt->bindParam(':nombre', $nombre_nuevo);
+			    $stmt->bindParam(':apellido', $apellido_nuevo);
+			    $stmt->bindParam(':estado', $estado_nuevo);
+			    $stmt->bindParam(':fechnac', $fechnac_nuevo);
+			    $stmt->bindParam(':nacionalidad_id', $nacionalidad_id_nuevo);
+			    $stmt->bindParam(':id', $id);
+			    //ejecutamos la consulta
+			    $stmt->execute();
+			} catch (PDOException $e) {
+			    echo 'Error de la coneccion a la BD:' . $e->getMessage();
+			    die();
+			}
 		}
     }
 
